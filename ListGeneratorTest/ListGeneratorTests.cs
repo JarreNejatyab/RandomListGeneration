7b96db7d40d372b4e7e6e93c1ffb00548b678295
Jarre Nejatyab <jarre.nejatyab@outlook.com>
2019-07-18 23:12:47 +1000

    cleaned up unit test

--- ListGeneratorTest/ListGeneratorTests.cs
+++ ListGeneratorTest/ListGeneratorTests.cs
@@ -11,25 +11,34 @@ namespace RandomListTest
         [Test]
         public void ListGenerationTest()
         {
+            //setup
+            const int min = 1;
+            const int max = 52;
+
             var random = new Mock<IRandom>();
 
-            random.Setup(random1 => random1.Random(It.IsAny<int>(), It.IsAny<int>())).Returns<int, int>((min, max) =>
-            {
-                var r = new Random();
-                return r.Next(min, max);
-            });
+            random.Setup(random1 => random1.Random(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns<int, int>((minMock, maxMock) =>
+                {
+                    var r = new Random();
+                    return r.Next(minMock, maxMock);
+                });
 
+            //act
             var sut = new ListGenerator(random.Object);
-            var result = sut.Generate(1,52);
+            var result = sut.Generate(min, max);
 
             //asserting efficiency of the algorithm 
-            random.Verify(random1 => random1.Random(It.IsAny<int>(),It.IsAny<int>()),Times.Exactly(52));
+            random.Verify(random1 => random1.Random(It.IsAny<int>(), It.IsAny<int>()), Times
+                .Exactly(max));
 
-            Assert.AreEqual(result.Count, 52);
+            Assert.AreEqual(result.Count, max);
+
             //asserting the uniqueness of items in the result
-            Assert.AreEqual(result.GroupBy(i => i).Count(), 52);
-            Assert.AreEqual(result.Min(), 1);
-            Assert.AreEqual(result.Max(), 52);
+            Assert.AreEqual(result.GroupBy(i => i).Count(), max);
+
+            Assert.AreEqual(result.Min(), min);
+            Assert.AreEqual(result.Max(), max);
         }
     }
 }
